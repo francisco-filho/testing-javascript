a2caec983bd9f5228465ab2907c51be5f7d834ce
francisco-filho <francisco@capimgrosso.com>
2017-03-29 22:44:24 -0300

    add webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path')
+
+const config = {
+  entry: {
+    'app': ['babel-polyfill', './src/app.js']
+  },
+  output: {
+    path: path.resolve(__dirname, 'public/'),
+    filename: 'app.js',
+    publicPath: 'public/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: [ path.resolve(__dirname, 'src')],
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  resolve: {
+    modules: [ 'node_modules'],
+    extensions: ['.js', '.jsx']
+  }
+}
+
+module.exports = config
