bc013bdb3b0dda19a8a05e090041587bf4e1713d
francisco-filho <francisco@capimgrosso.com>
2017-04-04 23:52:05 -0300

    add other extensions to find

--- public/img/convert-image.sh
+++ public/img/convert-image.sh
@@ -2,7 +2,7 @@
 
 DIR="500x"
 
-for i in $(find ./ -name "*.jpg" | grep -v ./$DIR ); do
+for i in $(find ./ \( -name "*.jpg" -or -name "*.png" -or -name "*.gif" \) | grep -v ./$DIR ); do
   echo "find $i"
   mkdir -p $(dirname "./$DIR${i##.}")
   gm convert "$i" -quality 70 -geometry 500x500 "./$DIR${i##.}"
