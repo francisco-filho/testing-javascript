dad13756ed37324ac6cf9d39715422896687f0f4
francisco-filho <francisco@capimgrosso.com>
2017-04-15 23:29:28 -0300

    add Filter & Pagination

--- src/components/DataGrid.js
+++ src/components/DataGrid.js
@@ -1,27 +1,105 @@
 import React, {Component} from 'react'
 
-export default class DataGrid extends Component {
+class Button extends Component {
+  render(){
+    return <a>Save {this.props.id}</a>
+  }
+}
+
+const Paginator = ({ currentPage, totalPages, onFirst, onLast, onNext, onPrevious }) => (
+  <div className="paginator">
+    <span className={`first ${currentPage <= 1 && 'disabled'}`} onClick={onFirst}>&lt;&lt;</span>
+    <span className={`previous ${currentPage <= 1 && 'disabled'}`} onClick={onPrevious}>&lt;</span>
+    <span className="status">{`${currentPage} de ${totalPages}`}</span>
+    <span className={`next ${currentPage >= totalPages && 'disabled'}`} onClick={onNext}>&gt;</span>
+    <span className={`last ${currentPage >= totalPages && 'disabled'}`} onClick={onLast}>&gt;&gt;</span>
+  </div>
+)
+
+class DataGrid extends Component {
   static get defaultProps(){
     return {
       columns: [],
       data: [],
-      defaultSort: null
+      defaultSort: null,
+      currentPage: 1,
+      filter: []
     }
   }
 
   constructor(){
     super()
-    this.state = {data: [], sortedBy: -1, reversed: false}
+    this.state = {
+      data: [],
+      sortedBy: -1,
+      reversed: false,
+      currentPage: 1
+    }
   }
 
-  componentDidMount(){
-    const { defaultSort, data } = this.props
+  initComponent(){
+    const { data, page, defaultSort } = this.props
     if (defaultSort  != null){
       const sorted = this.sort(data, defaultSort)
       this.setState({data: sorted.data, sortedBy: sorted.sortedBy })
     }
     else
-      this.setState({data})
+      this.setState({data, currentPage: page ? page : this.props.currentPage})
+  }
+
+  componentDidMount(){
+    this.initComponent()
+  }
+
+  getPage(data, page){
+    const { pageSize, paginate } = this.props
+    const from = (page * pageSize) - pageSize
+
+    return paginate ? data.slice(from, from + pageSize) : data
+  }
+
+  getPages(){
+    const pageCount = Math.ceil(this.props.data.length / this.props.pageSize)
+    return Array
+      .from(new Array(pageCount))
+      .map((p, i) => i + 1)
+  }
+
+  setPage(page){
+    if (page != this.state.currentPage)
+      this.setState({currentPage: page})
+  }
+
+  firstPage(){
+    const {currentPage} = this.state
+    const totalPages = this.getPages().length
+
+    if (currentPage > 1)
+      this.setPage(1)
+  }
+
+  previousPage(){
+    const {currentPage} = this.state
+    const totalPages = this.getPages().length
+
+    if (currentPage > 1)
+      this.setPage(currentPage - 1)
+  }
+
+  nextPage(){
+    const {currentPage} = this.state
+    const totalPages = this.getPages().length
+
+    if (currentPage < totalPages)
+      this.setPage(currentPage + 1)
+  }
+
+  lastPage(){
+    const {currentPage} = this.state
+    const totalPages = this.getPages().length
+
+    if (currentPage < totalPages)
+      this.setPage(totalPages)
   }
 
   sort(data, columnName){
@@ -47,37 +125,104 @@ export default class DataGrid extends Component {
     this.setState({data: sorted.data, sortedBy: sorted.sortedBy, reversed: sorted.reversed})
   }
 
+  zip(columns, row){
+    let c = columns
+    Object.keys(columns).forEach((k, i) => {
+      c[k] = row[i]
+    })
+    return c
+  }
+
+  filterData(value){
+    const { filter } = this.props
+    const idx = []
+    filter.forEach( (c, i) => {
+      const x = this.props.columns.findIndex( co => co.name === c)
+      if (x > -1)
+        idx.push(x)
+    })
+
+    const filteredData = this.state.data.filter(f => {
+      return idx.some((i) => {
+        return (""+f[i]).includes(value)
+      })
+    })
+    this.setState({data: filteredData})
+  }
+
+  clearFilter(){
+    this.initComponent()
+  }
+
   render(){
-    const { columns } = this.props
-    const { data } = this.state
+    const { columns, paginate, pageSize, filter } = this.props
+    const { data, currentPage } = this.state
+
+    let columnSchema = columns.map(c => c.name)
+    .reduce((o, n) => {
+      o[n] = null
+      return o
+    }, {})
+
     return (
-      <table>
-        <thead>
-        <tr>
+      <div className="datagrid">
+        <div className="header">
+          {
+            filter && <div className="filter">
+              <input type="text" onKeyDown={ e => {
+                if (e.keyCode == 13)
+                  this.filterData(e.target.value)
+              }}/>
+              <span className="close" onClick={e => this.clearFilter() }>x</span>
+            </div>
+          }
         {
-          columns.map((th, i) => {
-            return <th key={i} onClick={ e => {
-              this.sortBy(th.name)
-              e.preventDefault()
-              e.stopPropagation()
-            }}>{ th.name }</th>
-          })
-        }</tr>
-        </thead>
-        <tbody>
-        {
-          data.map((tr,i) => {
-            return <tr key={tr.id}>
-              {
-                columns.map((td,y) => {
-                  return <td key={y}>{tr[y]}</td>
-                })
-              }
-            </tr>
-          })
+          paginate && data.length > pageSize &&
+            <Paginator
+              currentPage={currentPage}
+              totalPages={this.getPages().length}
+              onFirst={ e => this.firstPage() }
+              onPrevious={ e => this.previousPage() }
+              onNext={ e => this.nextPage() }
+              onLast={ e => this.lastPage() } />
+
         }
-        </tbody>
-      </table>
+        </div>
+        <table>
+          <thead>
+          <tr>
+          {
+            columns.map((th, i) => {
+              return <th key={i} onClick={ e => {
+                this.sortBy(th.name)
+                e.preventDefault()
+                e.stopPropagation()
+              }}>{ th.name }</th>
+            })
+          }</tr>
+          </thead>
+          <tbody>
+          {
+            this.getPage(data, currentPage).map((tr,i) => {
+              return <tr key={i}>
+                {
+                  columns.map((td,y) => {
+                    return <td key={y}>{
+                      td.component ?
+                        React.createElement(td.component, this.zip(columnSchema, tr)) :
+                        tr[y]
+                    }</td>
+                  })
+                }
+              </tr>
+            })
+          }
+          </tbody>
+        </table>
+      </div>
     )
   }
 }
+
+export default DataGrid
+export { Button }
--- tests/DataGrid.spec.js
+++ tests/DataGrid.spec.js
@@ -13,79 +13,236 @@ function columnNameContent(tr, first, last){
   expect(tr.last().find('td').last().text()).to.be.equal(last)
 }
 
+class Button extends Component {
+  render(){
+    return <a onClick={ e => alert(this.props.name ) }>Save {this.props.id}</a>
+  }
+}
+
+class Check extends Component {
+  render(){
+    return <span>{ this.props.teen && <i className="fa fa-check"/> }</span>
+  }
+}
+
 describe('<DataGrid/>', () => {
 
   let columns = [], data = [], grid = null
 
-  beforeEach(()=> {
-    columns = [{name: 'id'}, {name: 'name'}]
-    data  = [[1,'julia'], [2,'joao'], [3, 'fernando']]
-    grid = mount(<DataGrid columns={columns} data={data} defaultSort={'name'}/>)
+  describe('Filter', () => {
+    let filter = null
+
+    beforeEach(() => {
+      columns = [{name: 'id'}, {name: 'name'}]
+      data = [[1, 'one'], [2, 'two1'], [3, 'three'], [4, 'four'],[5, 'five']]
+      grid = mount(<DataGrid columns={columns} data={data} filter={['id', 'name']}/>)
+      filter = grid.find('.filter')
+    })
+
+    it('Should render a input[text]', () => {
+      expect(filter.find('input[type="text"]')).to.have.length(1)
+    })
+
+    it('Should filter by name when enter a text in the inputbox', () => {
+      const input = filter.find('input')
+      input.simulate('keyDown', { keyCode: 13, target: { value: 'o'}})
+      expect(grid.find('tbody').find('tr')).to.have.length(3)
+    })
+
+    it('Should remove the filter when click "x"', () => {
+      const x = filter.find('.close')
+      x.simulate('click')
+      expect(grid.find('tbody').find('tr')).to.have.length(5)
+    })
+
+    it('Should filter by the letter "o"', () => {
+      const input = filter.find('input')
+      input.simulate('keyDown', { keyCode: 13, target: { value: 'o'}})
+
+      expect(grid.find('tbody').find('tr')).to.have.length(3)
+      expect(grid.find('tbody').find('tr').find('td').first().text()).to.be.equal('1')
+      expect(grid.find('tbody').find('tr').last().find('td').first().text()).to.be.equal('4')
+    })
   })
 
-  it('Should render two th"s', () => {
-    expect(grid.find('th')).to.have.length(2)
+  describe('Paginator', () => {
+    let paginator = null
+
+    beforeEach(() => {
+      columns = [{name: 'id'}, {name: 'name'}]
+      data = [[1, 'one'], [2, 'two'], [3, 'three'], [4, 'four'],[5, 'five']]
+      grid = mount(<DataGrid columns={columns} data={data} paginate pageSize={2}/>)
+      paginator = grid.find('.paginator')
+    })
+
+    it('Should render only 2 rows in the first page', () => {
+      expect(grid.find('tbody').find('tr')).to.have.length(2)
+    })
+
+    it('Should render only 2 rows in the second page', () => {
+      grid.instance().setPage(2)
+      expect(grid.find('tbody').find('tr')).to.have.length(2)
+    })
+
+    it('Should render only 1 row in the third page', () => {
+      grid.instance().setPage(3)
+      expect(grid.find('tbody').find('tr')).to.have.length(1)
+    })
+
+    it('Should render a paginator component', () => {
+      expect(grid.find('.paginator')).to.have.length(1)
+    })
+
+    it('Should render previous/next indicators', () => {
+      expect(paginator.find('span.first')).to.have.length(1)
+      expect(paginator.find('span.previous')).to.have.length(1)
+      expect(paginator.find('span.next')).to.have.length(1)
+      expect(paginator.find('span.last')).to.have.length(1)
+    })
+
+    it('Should render text "1 de 3"', () => {
+      expect(paginator.find('span.status').text()).to.be.equal('1 de 3')
+    })
+
+    it('Should mark page 3 as active', () => {
+      grid.instance().setPage(3)
+      expect(paginator.find('span.status').text()).to.be.equal('3 de 3')
+    })
+
+    it('Should change page on click the "next" indicator', () => {
+      paginator.find('.next').simulate('click')
+      expect(paginator.find('span.status').text()).to.be.equal('2 de 3')
+    })
+
+    it('Should change page on click the "last" indicator', () => {
+      paginator.find('.last').simulate('click')
+      expect(paginator.find('span.status').text()).to.be.equal('3 de 3')
+    })
+
+    it('Should change page on click the "previous" indicator', () => {
+      paginator.find('.last').simulate('click')
+      paginator.find('.previous').simulate('click')
+      expect(paginator.find('span.status').text()).to.be.equal('2 de 3')
+    })
+
+    it('Should change page on click the "first" indicator', () => {
+      paginator.find('.last').simulate('click')
+      paginator.find('.first').simulate('click')
+      expect(paginator.find('span.status').text()).to.be.equal('1 de 3')
+    })
+
+    it('Should disable first/previous indicator when in the first page', () => {
+      expect(paginator.find(".first.disabled")).to.have.length(1)
+      expect(paginator.find(".previous.disabled")).to.have.length(1)
+    })
+
+    it('Should disable last/next indicator when in the last page', () => {
+      paginator.find('.last').simulate('click')
+      expect(paginator.find(".next.disabled")).to.have.length(1)
+      expect(paginator.find(".last.disabled")).to.have.length(1)
+    })
   })
 
-  it('Should render two th"s with text [id, name]', () => {
-    const th = grid.find('th')
+  describe('Sub-components', () => {
+    beforeEach(() => {
+      columns = [{name: 'id'}, {name: 'name'}, { name: 'teen', component: Check}, {name: 'actions', component: Button}]
+      data = [[1, 'julia', false], [2, 'joao', false], [3, 'fernando', true]]
+      grid = mount(<DataGrid columns={columns} data={data} defaultSort={'id'}/>)
+    })
 
-    expect(th.first().text()).to.be.equal('id')
-    expect(th.last().text()).to.be.equal('name')
+    it('Should render a button Component in column', () => {
+      const tr = grid.find('tbody').find('tr').first()
+
+      expect(tr.find('a')).to.have.length(1)
+      expect(tr.find('a').text()).to.not.be.undefined
+    })
+
+    it('Should render button with the text "Save {id}"', () => {
+      const btn = grid.find('tbody').find('a').first()
+
+      expect(btn.text()).to.be.equal('Save 1')
+    })
+
+    it('Should render a icon if value of "teen" is true', () => {
+      const spanFalse = grid.find('tbody').find('span').first()
+      const spanTrue = grid.find('tbody').find('span').last()
+
+      expect(spanTrue.html()).to.contains("fa fa-check")
+      expect(spanFalse.html()).to.not.contains("fa fa-check")
+    })
   })
 
-  it('Should render a tbody with rows', () => {
-    const tbody = grid.find('tbody')
+  describe('Data & Sort', () => {
+    beforeEach(()=> {
+      columns = [{name: 'id'}, {name: 'name'}]
+      data  = [[1,'julia'], [2,'joao'], [3, 'fernando']]
+      grid = mount(<DataGrid columns={columns} data={data} defaultSort={'name'}/>)
+    })
 
-    expect(tbody.find('tr')).to.have.length(3)
-  })
+    it('Should render 2 th"s', () => {
+      expect(grid.find('th')).to.have.length(2)
+    })
 
-  it('Should sort data by the column "name"', () => {
-    grid = mount(<DataGrid columns={columns} data={data}/>)
+    it('Should render 2 th"s with text [id, name]', () => {
+      const th = grid.find('th')
 
-    grid.instance().sortBy('name')
-    const tr = grid.find('tbody').find('tr')
+      expect(th.first().text()).to.be.equal('id')
+      expect(th.last().text()).to.be.equal('name')
+    })
 
-    columnIdContent(tr, '3', '1')
-  })
+    it('Should render a tbody with rows', () => {
+      const tbody = grid.find('tbody')
 
-  it('Should sort in reverse order if columns is already sorted', () => {
-    grid = mount(<DataGrid columns={columns} data={data}/>)
-    grid.instance().sortBy('name')
-    grid.instance().sortBy('name')
+      expect(tbody.find('tr')).to.have.length(3)
+    })
 
-    const tr = grid.find('tbody').find('tr')
+    it('Should sort data by the column "name"', () => {
+      grid = mount(<DataGrid columns={columns} data={data}/>)
 
-    columnIdContent(tr, '1', '3')
-  })
+      grid.instance().sortBy('name')
+      const tr = grid.find('tbody').find('tr')
 
-  it('Should sort ascending when sorted by another column before', () => {
-    grid.instance().sortBy('name')
-    grid.instance().sortBy('id')
+      columnIdContent(tr, '3', '1')
+    })
 
-    const tr = grid.find('tbody').find('tr')
+    it('Should sort in reverse order if columns is already sorted', () => {
+      grid = mount(<DataGrid columns={columns} data={data}/>)
+      grid.instance().sortBy('name')
+      grid.instance().sortBy('name')
 
-    columnIdContent(tr, '1', '3')
-  })
+      const tr = grid.find('tbody').find('tr')
 
-  it('Should be sorted by default by "name"', ()=> {
-    const tr = grid.find('tbody').find('tr')
+      columnIdContent(tr, '1', '3')
+    })
 
-    columnIdContent(tr, '3', '1')
-    columnNameContent(tr, 'fernando', 'julia')
-  })
+    it('Should sort ascending when sorted by another column before', () => {
+      grid.instance().sortBy('name')
+      grid.instance().sortBy('id')
 
-  it('Should sort when click in "th"', ()=> {
-    grid.find('th').first().simulate('click')
+      const tr = grid.find('tbody').find('tr')
 
-    columnIdContent(grid.find('tbody').find('tr'), '1', '3')
-  })
+      columnIdContent(tr, '1', '3')
+    })
 
-  it('Should sort in reverse order when click three times in "th"', () => {
-    grid.find('th').first().simulate('click')
-    grid.find('th').first().simulate('click')
-    grid.find('th').first().simulate('click')
+    it('Should be sorted by default by "name"', ()=> {
+      const tr = grid.find('tbody').find('tr')
 
-    columnIdContent(grid.find('tbody').find('tr'), '1', '3')
+      columnIdContent(tr, '3', '1')
+      columnNameContent(tr, 'fernando', 'julia')
+    })
+
+    it('Should sort when click in "th"', ()=> {
+      grid.find('th').first().simulate('click')
+
+      columnIdContent(grid.find('tbody').find('tr'), '1', '3')
+    })
+
+    it('Should sort in reverse order when click 3 times in "th"', () => {
+      grid.find('th').first().simulate('click')
+      grid.find('th').first().simulate('click')
+      grid.find('th').first().simulate('click')
+
+      columnIdContent(grid.find('tbody').find('tr'), '1', '3')
+    })
   })
 })
